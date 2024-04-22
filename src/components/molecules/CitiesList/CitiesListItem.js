df0ab7eafc2c21205d1b234a9c17bcea40a4deae
Ward Wahhab <wahhab.ward@gmail.com>
2024-04-22 12:36:54 +0300

    Modified cities list item to accept data from parent

--- src/components/molecules/CitiesList/CitiesListItem.js
+++ src/components/molecules/CitiesList/CitiesListItem.js
@@ -1,32 +1,34 @@
 import React from "react";
 import Card from "../../atoms/Card/Card";
 import { Pressable, StyleSheet, View } from "react-native";
-import NoCity from "./NoCity";
 import AppText from "../../atoms/AppText/AppText";
 import { useNavigation } from "@react-navigation/native";
+import { colors } from "../../../../themes/styles/colors";
+import WeatherIcon from "../../atoms/WeatherIcon/WeatherIcon";
+import { formatTempBasedOnUnit } from "../../../../helpers/formatTemp";
+import { useSelector } from "react-redux";
 
-
-
-const CitiesListItem = ({ cities }) => {
+const CitiesListItem = ({ city }) => {
+  const unit = useSelector((state) => state.unit);
   const navigation = useNavigation();
   const cardPressHandler = () => {
-    navigation.navigate("CityWeather")
-  }
-
-  // TODO: Remove Comment
-  // if (!cities) {
-  //   return <NoCity />;
-  // }
+    navigation.navigate("CityWeather", {weatherData: city});
+  };
+  const name = city?.name;
+  const description=city?.weather[0]?.main;
+  const temperature=formatTempBasedOnUnit(city?.main?.temp, unit);
+  const icon=city?.weather[0]?.icon;
 
   return (
-    <Pressable style={({pressed}) => pressed && styles.pressed}>
+    <Pressable style={({ pressed }) => pressed && styles.pressed} onPress={cardPressHandler}>
       <Card style={styles.card}>
         <View style={styles.container}>
-          <AppText style={styles.title}>City</AppText>
-          <AppText style={styles.temperature}>15°</AppText>
+          <AppText style={styles.title}>{name}</AppText>
+          <AppText style={styles.text}>{temperature}°</AppText>
         </View>
         <View style={styles.brief}>
-          <AppText>Mostly Cloudy</AppText>
+          <WeatherIcon icon={icon} style={styles.icon} />
+          <AppText style={styles.text}>{description}</AppText>
         </View>
       </Card>
     </Pressable>
@@ -43,6 +45,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     backgroundColor: "#3a4251",
     padding: 8,
+    marginBottom: 12,
+    elevation: 2,
+    shadowColor: colors.primaryText,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
   },
   container: {
     flexDirection: "row",
@@ -55,13 +63,20 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
   },
-  temperature: {
-    fontSize: 18,
+  text: {
+    fontSize: 20,
   },
-  time: {},
   brief: {
+    flexDirection: "row",
+    alignItems: "center",
     alignSelf: "flex-end",
     marginHorizontal: 15,
     marginBottom: 5,
+    gap: 5,
+  },
+  icon: {
+    height: 24,
+    width: 24,
+    borderRadius: 12,
   },
 });
