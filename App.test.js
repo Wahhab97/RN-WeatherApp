1e7b09615c5660ffc6763f1b129f1ecf8db443f9
Ward Wahhab <wahhab.ward@gmail.com>
2024-04-24 15:47:14 +0300

    just to test

--- App.test.js
+++ App.test.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
 
-import App from './App';
+import App from "./App";
+// Import the modules you want to mock
+import { useDispatch, useSelector } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-describe('<App />', () => {
-  it('has 1 child', () => {
+// Mock the modules using Jest's jest.mock() function
+jest.mock("react-redux", () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
+jest.mock("redux-persist", () => {
+  const real = jest.requireActual("redux-persist");
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
+jest.mock("redux-persist/es/integration/react", () => ({
+  PersistGate: (props) => props.children,
+}));
+
+describe("<App />", () => {
+  it("has 1 child", () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
   });
